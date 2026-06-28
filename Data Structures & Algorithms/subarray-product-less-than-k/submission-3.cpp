44ee3cc985d3ffbb722b1d5c64c3aa775dc9f8d9
DhanushYarragunta <158791996+DhanushYarragunta@users.noreply.github.com>
2026-06-28 21:47:54 +0530

    Add: subarray-product-less-than-k - submission-3

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k <= 1) return 0;
+        int left = 0, ans = 0;
+        long long mul = 1;
+        for(int right = 0; right < nums.size(); right++)
+        {
+             mul *= nums[right];
+            while(left <= right && mul >= k)
+            {
+                mul /= nums[left];
+                left += 1;
+            }
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+};
